6a2d81496a51913699ce65e3f4b5248dee27c2cc
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-03-01 12:51:25 +0100

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Edwin Przeźwiecki Jr. on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
